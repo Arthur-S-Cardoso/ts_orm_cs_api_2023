3a2274552a6c0160a347a97c916ad5d204baaafd
Arthur Santos Cardoso <20201PF.INF_I0002@passofundo.ifsul.local>
2023-09-12 10:22:03 -0300

    second commit

--- src/routes.ts
+++ src/routes.ts
@@ -1,8 +1,10 @@
 import {Router} from 'express';
 import EnderecoController from './app/controllers/EnderecoController';
+import LocalController from './app/controllers/LocalController';
 
 const router = Router();
 
 router.get('/endereco/list', EnderecoController.list);
+router.get('/local/lis', LocalController.list);
 
 export default router;
